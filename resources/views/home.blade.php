b47ff6e57214d17fbf3b43049feacdffbeebcb3c
michaelsaiba84@gmail.com <michaelsaiba84@gmail.com>
2019-03-22 13:20:12 +0300

    Modifying the Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 			<header class="header">
 				<div class="logo-container">
 					<a href="../" class="logo">
-						<img src="assets/images/logo.png" height="35" alt="JSOFT Admin" />
+						<img src="assets/images/logo.png" height="35" alt="Project Manager logo" />
 					</a>
 					<div class="visible-xs toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
 						<i class="fa fa-bars" aria-label="Toggle sidebar"></i>
@@ -39,14 +39,14 @@
 							<div class="dropdown-menu notification-menu large">
 								<div class="notification-title">
 									<span class="pull-right label label-default">3</span>
-									Tasks
+									Pending Tasks
 								</div>
 
 								<div class="content">
 									<ul>
 										<li>
 											<p class="clearfix mb-xs">
-												<span class="message pull-left">Generating Sales Report</span>
+												<span class="message pull-left">Web Development</span>
 												<span class="message pull-right text-dark">60%</span>
 											</p>
 											<div class="progress progress-xs light">
@@ -56,7 +56,7 @@
 
 										<li>
 											<p class="clearfix mb-xs">
-												<span class="message pull-left">Importing Contacts</span>
+												<span class="message pull-left">User Interface Design</span>
 												<span class="message pull-right text-dark">98%</span>
 											</p>
 											<div class="progress progress-xs light">
@@ -66,7 +66,7 @@
 
 										<li>
 											<p class="clearfix mb-xs">
-												<span class="message pull-left">Uploading something big</span>
+												<span class="message pull-left">Back end for the android app</span>
 												<span class="message pull-right text-dark">33%</span>
 											</p>
 											<div class="progress progress-xs light mb-xs">
@@ -85,7 +85,7 @@
 
 							<div class="dropdown-menu notification-menu">
 								<div class="notification-title">
-									<span class="pull-right label label-default">230</span>
+									<span class="pull-right label label-default">12</span>
 									Messages
 								</div>
 
@@ -96,8 +96,8 @@
 												<figure class="image">
 													<img src="assets/images/!sample-user.jpg" alt="Joseph Doe Junior" class="img-circle" />
 												</figure>
-												<span class="title">Joseph Doe</span>
-												<span class="message">Lorem ipsum dolor sit.</span>
+												<span class="title">Mr. Kiprop</span>
+												<span class="message">Please submit your ERD diagrams</span>
 											</a>
 										</li>
 										<li>
@@ -105,28 +105,12 @@
 												<figure class="image">
 													<img src="assets/images/!sample-user.jpg" alt="Joseph Junior" class="img-circle" />
 												</figure>
-												<span class="title">Joseph Junior</span>
+												<span class="title">Mr. Biomdo</span>
 												<span class="message truncate">Truncated message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam, nec venenatis risus. Vestibulum blandit faucibus est et malesuada. Sed interdum cursus dui nec venenatis. Pellentesque non nisi lobortis, rutrum eros ut, convallis nisi. Sed tellus turpis, dignissim sit amet tristique quis, pretium id est. Sed aliquam diam diam, sit amet faucibus tellus ultricies eu. Aliquam lacinia nibh a metus bibendum, eu commodo eros commodo. Sed commodo molestie elit, a molestie lacus porttitor id. Donec facilisis varius sapien, ac fringilla velit porttitor et. Nam tincidunt gravida dui, sed pharetra odio pharetra nec. Duis consectetur venenatis pharetra. Vestibulum egestas nisi quis elementum elementum.</span>
 											</a>
 										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joe Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joe Junior</span>
-												<span class="message">Lorem ipsum dolor sit.</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joseph Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joseph Junior</span>
-												<span class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam.</span>
-											</a>
-										</li>
+
+
 									</ul>
 
 									<hr />
@@ -137,15 +121,16 @@
 								</div>
 							</div>
 						</li>
+
 						<li>
 							<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
 								<i class="fa fa-bell"></i>
-								<span class="badge">3</span>
+								<span class="badge">1</span>
 							</a>
 
 							<div class="dropdown-menu notification-menu">
 								<div class="notification-title">
-									<span class="pull-right label label-default">3</span>
+									<span class="pull-right label label-default">2</span>
 									Alerts
 								</div>
 
@@ -156,7 +141,7 @@
 												<div class="image">
 													<i class="fa fa-thumbs-down bg-danger"></i>
 												</div>
-												<span class="title">Server is Down!</span>
+												<span class="title">The android task is due</span>
 												<span class="message">Just now</span>
 											</a>
 										</li>
@@ -165,19 +150,11 @@
 												<div class="image">
 													<i class="fa fa-lock bg-warning"></i>
 												</div>
-												<span class="title">User Locked</span>
+												<span class="title">The UI design has been rejected</span>
 												<span class="message">15 minutes ago</span>
 											</a>
 										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<div class="image">
-													<i class="fa fa-signal bg-success"></i>
-												</div>
-												<span class="title">Connection Restaured</span>
-												<span class="message">10/10/2014</span>
-											</a>
-										</li>
+
 									</ul>
 
 									<hr />
@@ -198,18 +175,18 @@
 								<img src="assets/images/!logged-user.jpg" alt="Joseph Doe" class="img-circle" data-lock-picture="assets/images/!logged-user.jpg" />
 							</figure>
 							<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@JSOFT.com">
-								<span class="name">John Doe Junior</span>
-								<span class="role">administrator</span>
+								<span class="name">{{ Auth::user()->name }}</span>
+								<span class="role">Administrator</span>
 							</div>
 
-							<i class="fa custom-caret"></i>
+							<i class="fa fa-caret-down"></i>
 						</a>
 
 						<div class="dropdown-menu">
 							<ul class="list-unstyled">
 								<li class="divider"></li>
 								<li>
-									<a role="menuitem" tabindex="-1" href="pages-user-profile.html"><i class="fa fa-user"></i> My Profile</a>
+									<a role="menuitem" tabindex="-1" href="{{asset('octopus/pages-user-profile.html')}}"><i class="fa fa-user"></i> My Profile</a>
 								</li>
 								<li>
 									<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fa fa-lock"></i> Lock Screen</a>
@@ -242,19 +219,35 @@
 						<div class="nano-content">
 							<nav id="menu" class="nav-main" role="navigation">
 								<ul class="nav nav-main">
+
 									<li class="nav-active">
 										<a href="index.html">
 											<i class="fa fa-home" aria-hidden="true"></i>
 											<span>Dashboard</span>
 										</a>
 									</li>
-									<li>
-										<a href="mailbox-folder.html">
-											<span class="pull-right label label-primary">182</span>
-											<i class="fa fa-envelope" aria-hidden="true"></i>
-											<span>Mailbox</span>
-										</a>
-									</li>
+
+									<hr class="separator" />
+
+									<div class="sidebar-widget widget-tasks">
+										<div class="widget-header">
+											<h6>Admin Modules</h6>
+
+											<!--<div class="widget-toggle">+</div>-->
+
+										</div>
+										<div class="widget-content">
+											<ul class="list-unstyled m-none">
+												<li><a href="{{ route('companies.index') }}">Companies</a></li>
+												<li><a href="{{ route('projects.index') }}">Projects</a></li>
+												<li><a href="{{ route('tasks.index') }}">Tasks</a></li>
+											</ul>
+										</div>
+									</div>
+
+									<hr class="separator" />
+
+<!--
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-copy" aria-hidden="true"></i>
@@ -338,6 +331,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-tasks" aria-hidden="true"></i>
@@ -451,6 +445,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-list-alt" aria-hidden="true"></i>
@@ -489,6 +484,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-table" aria-hidden="true"></i>
@@ -527,6 +523,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-map-marker" aria-hidden="true"></i>
@@ -550,6 +547,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-columns" aria-hidden="true"></i>
@@ -578,6 +576,7 @@
 											</li>
 										</ul>
 									</li>
+
 									<li class="nav-parent">
 										<a>
 											<i class="fa fa-align-left" aria-hidden="true"></i>
@@ -611,42 +610,30 @@
 											</li>
 										</ul>
 									</li>
+-->
 									<li>
-										<a href="http://themeforest.net/item/JSOFT-responsive-html5-template/4106987?ref=JSOFT" target="_blank">
+										<a href="http://localhost:8000" target="_blank">
 											<i class="fa fa-external-link" aria-hidden="true"></i>
-											<span>Front-End <em class="not-included">(Not Included)</em></span>
+											<span>Home Page <em class="not-included">(Back to site's main page)</em></span>
 										</a>
 									</li>
+
 								</ul>
+
 							</nav>
 
-							<hr class="separator" />
-
-							<div class="sidebar-widget widget-tasks">
-								<div class="widget-header">
-									<h6>Projects</h6>
-									<div class="widget-toggle">+</div>
-								</div>
-								<div class="widget-content">
-									<ul class="list-unstyled m-none">
-										<li><a href="#">JSOFT HTML5 Template</a></li>
-										<li><a href="#">Tucson Template</a></li>
-										<li><a href="#">JSOFT Admin</a></li>
-									</ul>
-								</div>
-							</div>
 
 							<hr class="separator" />
 
 							<div class="sidebar-widget widget-stats">
 								<div class="widget-header">
-									<h6>Company Stats</h6>
+									<h6>User Statistics</h6>
 									<div class="widget-toggle">+</div>
 								</div>
 								<div class="widget-content">
 									<ul>
 										<li>
-											<span class="stats-title">Stat 1</span>
+											<span class="stats-title">Completed Tasks</span>
 											<span class="stats-complete">85%</span>
 											<div class="progress">
 												<div class="progress-bar progress-bar-primary progress-without-number" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">
@@ -655,7 +642,7 @@
 											</div>
 										</li>
 										<li>
-											<span class="stats-title">Stat 2</span>
+											<span class="stats-title">Pending Tasks</span>
 											<span class="stats-complete">70%</span>
 											<div class="progress">
 												<div class="progress-bar progress-bar-primary progress-without-number" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
@@ -663,8 +650,9 @@
 												</div>
 											</div>
 										</li>
+										<hr>
 										<li>
-											<span class="stats-title">Stat 3</span>
+											<span class="stats-title">Tasks That have meet their deadline</span>
 											<span class="stats-complete">2%</span>
 											<div class="progress">
 												<div class="progress-bar progress-bar-primary progress-without-number" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
@@ -699,101 +687,6 @@
 							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
 						</div>
 					</header>
-
-						<div class="row">
-						<div class="col-md-6">
-							<section class="panel">
-								<header class="panel-heading">
-									<div class="panel-actions">
-										<a href="#" class="fa fa-caret-down"></a>
-										<a href="#" class="fa fa-times"></a>
-									</div>
-
-									<h2 class="panel-title">Best Seller</h2>
-									<p class="panel-subtitle">Customize the graphs as much as you want, there are so many options and features to display information using JSOFT Admin Template.</p>
-								</header>
-								<div class="panel-body">
-
-									<!-- Flot: Basic -->
-									<div class="chart chart-md" id="flotDashBasic"></div>
-									<script>
-
-										var flotDashBasicData = [{
-											data: [
-												[0, 170],
-												[1, 169],
-												[2, 173],
-												[3, 188],
-												[4, 147],
-												[5, 113],
-												[6, 128],
-												[7, 169],
-												[8, 173],
-												[9, 128],
-												[10, 128]
-											],
-											label: "Series 1",
-											color: "#0088cc"
-										}, {
-											data: [
-												[0, 115],
-												[1, 124],
-												[2, 114],
-												[3, 121],
-												[4, 115],
-												[5, 83],
-												[6, 102],
-												[7, 148],
-												[8, 147],
-												[9, 103],
-												[10, 113]
-											],
-											label: "Series 2",
-											color: "#2baab1"
-										}, {
-											data: [
-												[0, 70],
-												[1, 69],
-												[2, 73],
-												[3, 88],
-												[4, 47],
-												[5, 13],
-												[6, 28],
-												[7, 69],
-												[8, 73],
-												[9, 28],
-												[10, 28]
-											],
-											label: "Series 3",
-											color: "#734ba9"
-										}];
-
-										// See: assets/javascripts/dashboard/examples.dashboard.js for more settings.
-
-									</script>
-
-								</div>
-							</section>
-						</div>
-						<div class="col-md-6">
-							<section class="panel">
-								<header class="panel-heading">
-									<div class="panel-actions">
-										<a href="#" class="fa fa-caret-down"></a>
-										<a href="#" class="fa fa-times"></a>
-									</div>
-									<h2 class="panel-title">Server Usage</h2>
-									<p class="panel-subtitle">It's easy to create custom graphs on JSOFT Admin Template, there are several graph types that you can use, such as lines, bars, pie charts, etc...</p>
-								</header>
-								<div class="panel-body">
-
-									<!-- Flot: Curves -->
-									<div class="chart chart-md" id="flotDashRealTime"></div>
-
-								</div>
-							</section>
-						</div>
-					</div>
 
 					<!-- start: page -->
 					<div class="row">
